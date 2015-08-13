b819269e859601b9d549361877d8f2986c1e947a
cao hebing <caohebing@caos-MacBook-Pro.local>
2015-08-13 15:39:16 +1000

    PartOne_exercise
    add one more text field

--- Dice_Rolling/ViewController.m
+++ Dice_Rolling/ViewController.m
@@ -10,9 +10,16 @@
 
 @interface ViewController ()
 
+//first dice number field
 @property (weak, nonatomic) IBOutlet UITextField *diceNumberInput;
 
+//result label
 @property (weak, nonatomic) IBOutlet UILabel *showResult;
+
+//second dice
+@property (weak, nonatomic) IBOutlet UITextField *diceTwoInput;
+
+
 
 @end
 
@@ -30,13 +37,41 @@
 
 - (IBAction)checkDiceNumber:(UIButton *)sender {
     
-    NSLog(@"%@", self.diceNumberInput.text);
+    NSLog(@"first dice: %@", self.diceNumberInput.text);
     
-    NSString *numberOfDice = self.diceNumberInput.text;
+    NSLog(@"second dice: %@", self.diceTwoInput.text);
     
-    NSUInteger diceInteger = [numberOfDice intValue];
+    NSString *numberOfDice1 = self.diceNumberInput.text;
     
-    //verify the input
+    NSString *numberOfDice2 = self.diceTwoInput.text;
+    
+    
+    //get integer value from string
+    NSUInteger diceInteger1 = [numberOfDice1 intValue];
+    
+    NSUInteger diceInteger2 = [numberOfDice2 intValue];
+    
+    
+    if ( diceInteger1 >0 && diceInteger1 < 7 &&diceInteger2 >0 && diceInteger2 <7)
+    {
+        
+        
+        NSUInteger totalNumber = diceInteger1 + diceInteger2;
+        
+        NSString *total = [NSString stringWithFormat:@"%lu", totalNumber];
+        
+       
+        [self.showResult setText:total];
+        
+    }else{
+        
+        [self.showResult setText:@"Please Enter 1 - 6"];
+        
+    }
+    
+    
+    
+  /*  //verify the input
     if ( diceInteger > 3 && diceInteger <=6)
     {
         [self.showResult setText:@"Big"];
@@ -49,7 +84,7 @@
         [self.showResult setText:@"Please Enter 1 - 6"];
     }
     
-    //[self.diceResult setText:numberOfDice];
+    //[self.diceResult setText:numberOfDice];*/
 
 }
 
