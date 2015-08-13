78117115b6a40c9b80a72829bf8831d0573d5a3a
cao hebing <caohebing@caos-MacBook-Pro.local>
2015-08-13 13:51:10 +1000

    Initial Commit

---
+++ Dice_Rolling/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dice_Rolling
+//
+//  Created by cao hebing on 13/08/2015.
+//  Copyright (c) 2015 Lisa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Dice_Rolling/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dice_Rolling
+//
+//  Created by cao hebing on 13/08/2015.
+//  Copyright (c) 2015 Lisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
